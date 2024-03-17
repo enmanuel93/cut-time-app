066838d030b64d5417ccdbc5aea28034b5ccc994
Francisco Enmanuel Cruz Mejia <emmanuelmejia30@hotmail.com>
2024-03-17 23:05:57 +0100

    add web project

---
+++ CutTimeApp/CutTime.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CutTime.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

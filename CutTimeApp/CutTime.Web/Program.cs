64d16fd3830e0e58367670b8f2c0c88d9dd6029c
Francisco Enmanuel Cruz Mejia <emmanuelmejia30@hotmail.com>
2024-03-19 20:38:12 +0100

    add template

--- CutTimeApp/CutTime.Web/Program.cs
+++ CutTimeApp/CutTime.Web/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Razor.Runtime;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 

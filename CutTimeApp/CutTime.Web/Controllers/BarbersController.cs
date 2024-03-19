799da2ae8dece8a3bf527d3bcc40d91a206eea38
Francisco Enmanuel Cruz Mejia <emmanuelmejia30@hotmail.com>
2024-03-19 23:43:16 +0100

    add client, barbers and appointment ui

---
+++ CutTimeApp/CutTime.Web/Controllers/BarbersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CutTime.Web.Controllers
+{
+    public class BarbersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

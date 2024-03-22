515e151ceb02c5814013a312818f32d0e4d072c2
Francisco Enmanuel Cruz Mejia <emmanuelmejia30@hotmail.com>
2024-03-22 14:34:02 +0100

    Add db connection service

--- CutTimeApp/CutTime.Web/Program.cs
+++ CutTimeApp/CutTime.Web/Program.cs
@@ -1,7 +1,13 @@
+using CutTime.Domain.Models;
 using CutTime.Web.Helpers;
 using Microsoft.AspNetCore.Razor.Runtime;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CutTimeContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("CutTimeContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
